9b167b81fba2acdd282846c873036fee0a39a874
FO-nTTaX <fo.nttax@yahoo.de>
2017-03-27 15:24:44 +0200

    add sql file

---
+++ liquipedia_mediawiki_messages.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `liquipedia_mediawiki_messages` (
+`id` int(11) NOT NULL,
+  `messagename` varchar(255) NOT NULL,
+  `messagevalue` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `liquipedia_mediawiki_messages`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `messagename` (`messagename`);
+
+ALTER TABLE `liquipedia_mediawiki_messages`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
