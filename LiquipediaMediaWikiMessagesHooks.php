a1485d387cd1566f490e22c5a3b8d1f5eec92d90
FO-nTTaX <fo.nttax@yahoo.de>
2017-07-25 13:38:52 +0200

    switched to mediawiki DB objects and added exception handling

--- LiquipediaMediaWikiMessagesHooks.php
+++ LiquipediaMediaWikiMessagesHooks.php
@@ -2,17 +2,17 @@
 
 class LiquipediaMediaWikiMessagesHooks {
 	public static function onMessagesPreLoad( $title, &$message ) {
+		global $wgDBname;
 		$tablename = LiquipediaMediaWikiMessagesHelper::getTableName();
-		$dbo = LiquipediaMediaWikiMessagesHelper::getDBO();
-		if($dbo == null) {
-			return;
-		}
-		$sql = "SELECT * FROM `" . $tablename . "` WHERE `messagename` = :messagename";
-		$pdostatement = $dbo->prepare( $sql );
-		$pdostatement->execute( [':messagename' => $title] );
-		$result = $pdostatement->fetch();
-		if( $result ) {
-			$message = $result['messagevalue'];
+		try {
+			$dbr = wfGetDB( DB_REPLICA, '', $wgDBname );
+			$res = $dbr->select( $tablename, ['messagevalue'], ['messagename' => $title] );
+			if( $res->numRows() === 1 ) {
+				$row = $res->fetchObject();
+				$message = $row->messagevalue;
+			}
+		} catch(Exception $e) {
+			//echo $e->getMessage();
 		}
 	}
 }
