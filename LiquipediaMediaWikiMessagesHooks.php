76362d1429ae93e3173413c731eeb545280cd1d9
FO-nTTaX <fo.nttax@yahoo.de>
2017-10-24 15:35:12 +0200

    mediawiki database access

--- LiquipediaMediaWikiMessagesHooks.php
+++ LiquipediaMediaWikiMessagesHooks.php
@@ -3,10 +3,9 @@
 class LiquipediaMediaWikiMessagesHooks {
 	public static function onMessagesPreLoad( $title, &$message ) {
 		global $wgDBname;
-		$tablename = LiquipediaMediaWikiMessagesHelper::getTableName();
 		try {
 			$dbr = wfGetDB( DB_REPLICA, '', $wgDBname );
-			$res = $dbr->select( $tablename, [ 'messagevalue' ], [ 'messagename' => $title ] );
+			$res = $dbr->select( 'liquipedia_mediawiki_messages', [ 'messagevalue' ], [ 'messagename' => $title ] );
 			if( $res->numRows() === 1 ) {
 				$obj = $res->fetchObject();
 				$message = $obj->messagevalue;
--- SpecialLiquipediaMediaWikiMessages.php
+++ SpecialLiquipediaMediaWikiMessages.php
@@ -11,38 +11,38 @@ class SpecialLiquipediaMediaWikiMessages extends SpecialPage {
 	}
 
 	function execute( $par ) {
+		global $wgDBname;
 		if ( !$this->userCanExecute( $this->getUser() ) ) {
 			$this->displayRestrictionError();
 			return;
 		}
-		$tablename = LiquipediaMediaWikiMessagesHelper::getTableName();
+		$tablename = 'liquipedia_mediawiki_messages';
 		$params = explode( '/', $par );
 		$output = $this->getOutput();
 		$output->addModules( 'ext.liquipediamediawikimessages.SpecialPage' );
 		$this->setHeaders();
 		$request = $this->getRequest();
-		$dbo = LiquipediaMediaWikiMessagesHelper::getDBO();
+		$dbw = wfGetDB( DB_MASTER, '', $wgDBname );
 		if($params[0] == 'new') {
-			$output->addHTML( '<h2>' . $this->msg( 'liquipediamediawikimessages-add-new-message' )->text() . '</h2>' );
+			$output->addWikiText( '<h2>' . $this->msg( 'liquipediamediawikimessages-add-new-message' )->text() . '</h2>' );
 			$reqMessage = ucfirst( strtolower( $request->getText( 'reqmessage' ) ) );
 			$reqValue = $request->getText( 'reqvalue' );
 			if ( $request->getBool( 'createnew' ) ) {
 				if( !empty( $reqMessage ) && !empty( $reqValue ) ) {
-					$sql = "INSERT INTO `" . $tablename . "` (`messagename`, `messagevalue`) VALUES (:messagename, :messagevalue)";
 					try {
-						$dbo->prepare($sql)->execute( [ ':messagename' => $reqMessage, ':messagevalue' => $reqValue ] );
-						$output->addHTML( '<div class="alert alert-success">' . $this->msg( 'liquipediamediawikimessages-add-new-message-success' )->text() . '</div>' );
+						$dbw->insert( 'liquipedia_mediawiki_messages', array( 'messagename' => $reqMessage, 'messagevalue' => $reqValue ) );
+						$output->addWikiText( '<div class="alert alert-success">' . $this->msg( 'liquipediamediawikimessages-add-new-message-success' )->text() . '</div>' );
 						$reqMessage = '';
 						$reqValue = '';
 					} catch( PDOException $e ) {
 						if( $e->getCode() == 23000 ) {
-							$output->addHTML( '<div class="alert alert-danger">' . $this->msg( 'liquipediamediawikimessages-add-new-message-error-duplicate' )->text() . '</div>' );
+							$output->addWikiText( '<div class="alert alert-danger">' . $this->msg( 'liquipediamediawikimessages-add-new-message-error-duplicate' )->text() . '</div>' );
 						} else {
-							$output->addHTML( '<div class="alert alert-danger">' . $this->msg( 'liquipediamediawikimessages-add-new-message-error-unknown' )->text() . '</div>' );
+							$output->addWikiText( '<div class="alert alert-danger">' . $this->msg( 'liquipediamediawikimessages-add-new-message-error-unknown' )->text() . '</div>' );
 						}
 					}
 				} else {
-					$output->addHTML( '<div class="alert alert-danger">' . $this->msg( 'liquipediamediawikimessages-add-new-message-empty' )->text() . '</div>' );
+					$output->addWikiText( '<div class="alert alert-danger">' . $this->msg( 'liquipediamediawikimessages-add-new-message-empty' )->text() . '</div>' );
 				}
 			}
 			$output->addHTML( '<form name="newliquipediamediawikimessagesmessage" method="post">
@@ -66,25 +66,20 @@ class SpecialLiquipediaMediaWikiMessages extends SpecialPage {
 				</table>
 			</form>' );
 		} elseif( ( $params[0] == 'edit' ) && isset( $params[1] ) && !empty( $params[1] ) ) {
-			$output->addHTML( '<h2>' . $this->msg( 'liquipediamediawikimessages-edit-message' )->text() . '</h2>' );
-			$sql = "SELECT * FROM `" . $tablename . "` WHERE `id` = :id";
-			$pdostatement = $dbo->prepare( $sql );
-			$pdostatement->execute( [':id' => $params[1]] );
-			$result = $pdostatement->fetch();
+			$output->addWikiText( '<h2>' . $this->msg( 'liquipediamediawikimessages-edit-message' )->text() . '</h2>' );
+			$result = get_object_vars( $dbw->selectRow( 'liquipedia_mediawiki_messages', '*', array( 'id' => $params[1] ) ) );
 			if( $result ) {
 				$reqValue = $result['messagevalue'];
 				if ( $request->getBool( 'editmessage' ) ) {
 					$reqValue = $request->getText( 'reqvalue' );
-					$sql = "UPDATE `" . $tablename . "` SET `messagevalue` = :messagevalue WHERE `id` = :id";
-					$pdostatement = $dbo->prepare( $sql );
-					$pdostatement->execute( [':id' => $params[1], ':messagevalue' => $reqValue] );
-					$output->addHTML( '<div class="alert alert-success">' . $this->msg( 'liquipediamediawikimessages-edit-message-success' )->text() . '</div>' );
+					$dbw->update( 'liquipedia_mediawiki_messages', array( 'messagevalue' => $reqValue ), array( 'id' => $params[1] ) );
+					$output->addWikiText( '<div class="alert alert-success">' . $this->msg( 'liquipediamediawikimessages-edit-message-success' )->text() . '</div>' );
 				}
 				$output->addHTML( '<form name="newliquipediamediawikimessagesmessage" method="post">
 					<table>
 						<tr>
 							<td class="input-label"><label>' . $this->msg( 'liquipediamediawikimessages-message' )->text() . '</label></td>
-							<td class="input-container"><code>' . $result['messagename'] . '</code></td>
+							<td class="input-container"><code>' . htmlspecialchars( $result['messagename'] ) . '</code></td>
 							<td class="input-helper"></td>
 						</tr>
 						<tr>
@@ -101,39 +96,31 @@ class SpecialLiquipediaMediaWikiMessages extends SpecialPage {
 					</table>
 				</form>' );
 			} else {
-				$output->addHTML( '<div class="alert alert-warning">' . $this->msg( 'liquipediamediawikimessages-edit-message-nonexistent' )->text() . '</div>' );
+				$output->addWikiText( '<div class="alert alert-warning">' . $this->msg( 'liquipediamediawikimessages-edit-message-nonexistent' )->text() . '</div>' );
 			}
 		} elseif( ( $params[0] == 'delete' ) && isset( $params[1] ) && !empty( $params[1] ) ) {
-			$output->addHTML( '<h2>' . $this->msg( 'liquipediamediawikimessages-delete-message' )->text() . '</h2>' );
+			$output->addWikiText( '<h2>' . $this->msg( 'liquipediamediawikimessages-delete-message' )->text() . '</h2>' );
 			if ( $request->getBool( 'deletemessage' ) ) {
-				$sql = "DELETE FROM `" . $tablename . "` WHERE `id` = :id";
-				$pdostatement = $dbo->prepare( $sql );
-				$pdostatement->execute( [':id' => $params[1]] );
-				$output->addHTML( '<div class="alert alert-success">' . $this->msg( 'liquipediamediawikimessages-delete-message-success' )->text() . '</div>' );
+				$dbw->delete( 'liquipedia_mediawiki_messages', array( 'id' => $params[1] ) );
+				$output->addWikiText( '<div class="alert alert-success">' . $this->msg( 'liquipediamediawikimessages-delete-message-success' )->text() . '</div>' );
 			} else {
-				$sql = "SELECT * FROM `" . $tablename . "` WHERE `id` = :id";
-				$pdostatement = $dbo->prepare( $sql );
-				$pdostatement->execute( [':id' => $params[1]] );
-				$result = $pdostatement->fetch();
+				$result = get_object_vars( $dbw->selectRow( 'liquipedia_mediawiki_messages', '*', array( 'id' => $params[1] ) ) );
 				if( $result ) {
-					$output->addHTML( '<div class="alert alert-danger">' . $this->msg( 'liquipediamediawikimessages-delete-message-confirm' )->text() . '</div>' );
-					$output->addHTML( '<div>' . $this->msg( 'liquipediamediawikimessages-message' )->text() . ' <code>' . $result['messagename'] . '</code></div>' );
-					$output->addHTML( '<div>' . $this->msg( 'liquipediamediawikimessages-value' )->text() . ' <pre>' . $result['messagevalue'] . '</pre></div>' );
-					$output->addHTML( '<form name="deleteliquipediamediawikimessagesmessage" method="post"><input class="btn btn-danger" type="submit" name="deletemessage" value="' . $this->msg( 'liquipediamediawikimessages-delete-message-delete-button' )->text() . '"></form>' );
+					$output->addWikiText( '<div class="alert alert-danger">' . $this->msg( 'liquipediamediawikimessages-delete-message-confirm' )->text() . '</div>' );
+					$output->addWikiText( '<div>' . $this->msg( 'liquipediamediawikimessages-message' )->text() . ' <code>' . $result['messagename'] . '</code></div>' );
+					$output->addWikiText( '<div>' . $this->msg( 'liquipediamediawikimessages-value' )->text() . ' <pre>' . $result['messagevalue'] . '</pre></div>' );
+					$output->addWikiText( '<form name="deleteliquipediamediawikimessagesmessage" method="post"><input class="btn btn-danger" type="submit" name="deletemessage" value="' . $this->msg( 'liquipediamediawikimessages-delete-message-delete-button' )->text() . '"></form>' );
 				} else {
-					$output->addHTML( '<div class="alert alert-warning">' . $this->msg( 'liquipediamediawikimessages-delete-message-nonexistent' )->text() . '</div>' );
+					$output->addWikiText( '<div class="alert alert-warning">' . $this->msg( 'liquipediamediawikimessages-delete-message-nonexistent' )->text() . '</div>' );
 				}
 			}
 		}
-		$output->addHTML( '<h2>' . $this->msg( 'liquipediamediawikimessages-all-messages' )->text() . '</h2>' );
+		$output->addWikiText( '<h2>' . $this->msg( 'liquipediamediawikimessages-all-messages' )->text() . '</h2>' );
 		$output->addWikiText( '<div>[[Special:LiquipediaMediaWikiMessages/new|' . $this->msg( 'liquipediamediawikimessages-add-new-message-link' )->text() . ']]</div>' );
-		$sql = "SELECT * FROM `" . $tablename . "`";
-		$pdostatement = $dbo->prepare( $sql );
-		$pdostatement->execute();
-		$result = $pdostatement->fetchAll();
+		$res = $dbw->select( 'liquipedia_mediawiki_messages', '*', '' );
 		$table = '{| class="wikitable sortable"' . "\n";
 		$table .= "|-\n!" . $this->msg( 'liquipediamediawikimessages-column-id' )->text() . "\n!" . $this->msg( 'liquipediamediawikimessages-column-name' )->text() . "\n!" . $this->msg( 'liquipediamediawikimessages-column-value' )->text() . "\n!" . $this->msg( 'liquipediamediawikimessages-column-edit' )->text() . "\n!" . $this->msg( 'liquipediamediawikimessages-column-delete' )->text() . "\n";
-		foreach($result as $row) {
+		while( $row = $res->fetchRow() ) {
 			$table .= "|-\n|" . $row['id'] . "\n|" . $row['messagename'] . "\n|<pre>" . $row['messagevalue'] . "</pre>\n|[[Special:LiquipediaMediaWikiMessages/edit/" . $row['id'] . '|edit]]' . "\n|[[Special:LiquipediaMediaWikiMessages/delete/" . $row['id'] . '|delete]]' . "\n";
 		}
 		$table .= '|}';
