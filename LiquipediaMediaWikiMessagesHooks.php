e45d7ffa1d2181d3314cebc7393748317e8f6ff8
FO-nTTaX <fo.nttax@yahoo.de>
2017-11-27 15:58:07 +0100

    cut down a lot on duplicate database queries

--- LiquipediaMediaWikiMessagesHooks.php
+++ LiquipediaMediaWikiMessagesHooks.php
@@ -1,18 +1,28 @@
 <?php
 
 class LiquipediaMediaWikiMessagesHooks {
+	private static $messageCache = array();
 	public static function onMessagesPreLoad( $title, &$message ) {
 		global $wgDBname;
-		try {
-			$dbr = wfGetDB( DB_REPLICA, '', $wgDBname );
-			$res = $dbr->select( 'liquipedia_mediawiki_messages', [ 'messagevalue' ], [ 'messagename' => $title ] );
-			if( $res->numRows() === 1 ) {
-				$obj = $res->fetchObject();
-				$message = $obj->messagevalue;
+		if( isset( self::$messageCache[$title] ) && self::$messageCache[$title] === false ) {
+			return;
+		} elseif( isset( self::$messageCache[$title] ) ) {
+			$message = self::$messageCache[$title];
+		} else {
+			try {
+				$dbr = wfGetDB( DB_REPLICA, '', $wgDBname );
+				$res = $dbr->select( 'liquipedia_mediawiki_messages', [ 'messagevalue' ], [ 'messagename' => $title ] );
+				if( $res->numRows() === 1 ) {
+					$obj = $res->fetchObject();
+					self::$messageCache[$title] = $obj->messagevalue;
+					$message = $obj->messagevalue;
+				} else {
+					self::$messageCache[$title] = false;
+				}
+				$res->free();
+			} catch( Exception $e ) {
+				//echo $e->getMessage();
 			}
-			$res->free();
-		} catch( Exception $e ) {
-			//echo $e->getMessage();
 		}
 	}
 }
