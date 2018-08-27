9ca23e30fbe04ac9c0f1e1317d1b9d845d467e1b
FO-nTTaX <fo.nttax@yahoo.de>
2018-08-27 18:11:18 +0200

    add another fallback layer for other languages

--- LiquipediaMediaWikiMessagesHooks.php
+++ LiquipediaMediaWikiMessagesHooks.php
@@ -4,23 +4,46 @@ class LiquipediaMediaWikiMessagesHooks {
 
 	private static $messageCache = array();
 
-	public static function onMessagesPreLoad( $title, &$message ) {
+	public static function onMessagesPreLoad( $title, &$message, $code ) {
 		global $wgDBname;
-		if ( isset( self::$messageCache[ $title ] ) && self::$messageCache[ $title ] === false ) {
-			return;
-		} elseif ( isset( self::$messageCache[ $title ] ) ) {
-			$message = self::$messageCache[ $title ];
-		} else {
-			$dbr = wfGetDB( DB_REPLICA, '', $wgDBname );
-			$res = $dbr->select( 'liquipedia_mediawiki_messages', [ 'messagevalue' ], [ 'messagename' => $title ] );
-			if ( $res->numRows() === 1 ) {
-				$obj = $res->fetchObject();
-				self::$messageCache[ $title ] = $obj->messagevalue;
-				$message = $obj->messagevalue;
+		$bareTitle = str_replace( '/' . $code, '', $title );
+		$language = $code;
+		$found = false;
+		$last = false;
+		$dbr = wfGetDB( DB_REPLICA, '', $wgDBname );
+		while ( !$found ) {
+			if ( !$last ) {
+				$usedTitle = $bareTitle . '/' . $language;
 			} else {
-				self::$messageCache[ $title ] = false;
+				$usedTitle = $bareTitle;
 			}
-			$res->free();
+			if ( isset( self::$messageCache[ $usedTitle ] ) && self::$messageCache[ $usedTitle ] === false ) {
+				return;
+			} elseif ( isset( self::$messageCache[ $usedTitle ] ) ) {
+				$message = self::$messageCache[ $usedTitle ];
+				$found = true;
+			} else {
+				$res = $dbr->select( 'liquipedia_mediawiki_messages', [ 'messagevalue' ], [ 'messagename' => $usedTitle ] );
+				if ( $res->numRows() === 1 ) {
+					$obj = $res->fetchObject();
+					self::$messageCache[ $usedTitle ] = $obj->messagevalue;
+					$message = $obj->messagevalue;
+					$found = true;
+				}
+				$res->free();
+				if ( $last ) {
+					break;
+				}
+				$languages = Language::getFallbacksFor( $language );
+				if ( count( $languages ) > 0 ) {
+					$language = $languages[ 0 ];
+				} else {
+					$last = true;
+				}
+			}
+		}
+		if ( !$found ) {
+			self::$messageCache[ $title ] = false;
 		}
 	}
 
