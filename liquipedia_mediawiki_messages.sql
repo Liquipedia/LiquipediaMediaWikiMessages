801bf546cc4afc1919248cbb3476fc2aeaa2d582
FO-nTTaX <fo.nttax@yahoo.de>
2020-01-06 16:21:12 +0100

    utf8 changes

--- liquipedia_mediawiki_messages.sql
+++ liquipedia_mediawiki_messages.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `liquipedia_mediawiki_messages` (
 `id` int(11) NOT NULL,
   `messagename` varchar(255) NOT NULL,
   `messagevalue` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `liquipedia_mediawiki_messages`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `messagename` (`messagename`);
